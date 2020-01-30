344c88fdf1338846e7ffa6daacedd388984d1f6b
Nitay Hason <nitay.has@gmail.com>
2020-01-30 02:04:28 +0200

    fix

--- Tests/test_svm.py
+++ Tests/test_svm.py
@@ -3,26 +3,36 @@ import numpy as np
 import pandas as pd
 import sys, os
 import time
+import random
 
 from Models.SVM import SVM
 
 
-y_column_idx   = 17
 features_check = {
 	"base": {
-		"features": [1,2,3,4,5,6,7,8,9]
+		"features"	   : [1,2,3,4,5,6,7,8,9],
+		"y_column_idx" : 10,
+		"feature_file" : "../Datasets/features_extractions/base_(all).csv"
 	},
 	"base_robust": {
-		"features": [2,6,8,9]
+		"features": [2,6,8,9],
+		"y_column_idx" : 10,
+		"feature_file" : "../Datasets/features_extractions/base_(all).csv"
 	},
 	"all": {
-		"features": [1,2,3,4,5,6,7,8,9,10,11,13,15]
+		"features": [1,2,3,4,5,6,7,8,9,10,11,13,15],
+		"y_column_idx" : 17,
+		"feature_file" : "../Datasets/features_extractions/median_9_2_(25-75)_vt_include.csv"
 	},
 	"novel": {
-		"features": [10,11,13,15]
+		"features": [10,11,13,15],
+		"y_column_idx" : 17,
+		"feature_file" : "../Datasets/features_extractions/median_9_2_(25-75)_vt_include.csv"
 	},
 	"hybrid_robust": {
-		"features": [2,6,8,9,10,11,13,15]
+		"features": [2,6,8,9,10,11,13,15],
+		"y_column_idx" : 17,
+		"feature_file" : "../Datasets/features_extractions/median_9_2_(25-75)_vt_include.csv"
 	}
 }
 
@@ -38,14 +48,20 @@ gamma            = 2
 test_size		 = 0.25
 
 path               = os.path.dirname(os.path.abspath(__file__))
-features_file_name = "../Datasets/features_extractions/median_9_2_(75-25)_vt_include.csv"
-features_file      = os.path.join(path, features_file_name)
 
 for features_set in features_to_check:
 	print("\n\nChecking features - %s" % (features_set))
-	start       = time.time()
-	df          = pd.read_csv(features_file)
-	use_columns = features_check[features_set]["features"]
+	features_file = os.path.join(path, features_check[features_set]["feature_file"])
+	y_column_idx  = features_check[features_set]["y_column_idx"]
+	start         = time.time()
+	df            = pd.read_csv(features_file)
+	######## Append artificial data by number of consecutive characters feature ########
+	if 2 in features_check[features_set]["features"]:
+		mal         = df[df[df.columns[y_column_idx]]==1].sample(500).copy()
+		mal["2"]    = mal["2"].apply(lambda x:x*random.randint(3,9))
+		df = df.append(mal, ignore_index=True)
+	######################################## END #######################################
+	use_columns   = features_check[features_set]["features"]
 	use_columns.append(y_column_idx)
 
 	new_df = df[df.columns[use_columns]]
